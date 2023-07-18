e7b466c972b054f81f2a417a4e6b7ab5252b82b2
anastasiyaArkhipchuk <nastyashvec42@gmail.com>
2023-07-18 15:44:42 +0300

    verstka 1

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import {Button} from "./Button";
 
 function App() {
     let [count, setCount] = useState<number>(0)
+    let [maxValue, setMaxValue] = useState<number>(0)
+    let [startValue, setStartValue] = useState<number>(0)
     const CountPlus = () => {
         let newCount = count + 1
         setCount(newCount)
@@ -11,30 +13,46 @@ function App() {
     const CountReset = () => {
         setCount(0)
     }
-    const CountPlusDisable = count === 5
-    const CountResetDisable = count === 0
+    const CountPlusDisable = count === maxValue
+    const CountResetDisable = count === startValue
     return (
         <div className={'all'}>
-            <div className={'appWrapper'}>
-            <div className={count == 5
-                ? 'titleDisable'
-                : 'title'}> {count}
-            </div>
+            <div className={'settingsAll'}>
+                <div className={'settings'}>
+                    <div className={'titleSettings'}>
+                        <div>max value: <input type={"number"}/></div>
+                        <div>start value: <input type={"number"}/></div>
+                    </div>
+                    <div className={'buttonsSettings'}>
+                        <Button title={'SET'} callback={() => {
+                        }} disabled={CountResetDisable}/>
+                    </div>
+                </div>
 
-            <div className={'buttons'}>
-                <Button
-                    title={"INC"}
-                    callback={CountPlus}
-                    disabled={CountPlusDisable}
-                />
-                <Button
-                    title={"RESET"}
-                    callback={CountReset}
-                    disabled={CountResetDisable}
-                />
+            </div>
+            <div className={'counterAll'}>
+                <div className={'counter'}>
+                    <div className={count == 5
+                        ? 'titleDisable'
+                        : 'title'}> {count}
+                    </div>
+
+                    <div className={'buttons'}>
+                        <Button
+                            title={"INC"}
+                            callback={CountPlus}
+                            disabled={CountPlusDisable}
+                        />
+                        <Button
+                            title={"RESET"}
+                            callback={CountReset}
+                            disabled={CountResetDisable}
+                        />
+                    </div>
+                </div>
             </div>
         </div>
-        </div>
+
     );
 }
 
